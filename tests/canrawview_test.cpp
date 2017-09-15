fe66a0d5683133b0b6ab6856bf6615ad388a34a4
rthrh <woeflele@gmail.com>
2017-09-15 11:10:55 +0200

    unit tests continued

--- tests/canrawview_test.cpp
+++ tests/canrawview_test.cpp
@@ -1,17 +1,31 @@
-#include <QJsonArray>
+#define CATCH_CONFIG_RUNNER
+#include <QSignalSpy>
 #include <QStandardItemModel>
-#include <../src/components/canrawview/canrawview.h>
+#include <QtWidgets/QApplication>
+
 #include <context.h>
 #include <fakeit.hpp>
-#include <../src/components/canrawview/gui/crvguiinterface.h>
-#include <../src/components/canrawview/crvfactoryinterface.h>
-#include <memory>
 
+#include <memory>
+#include <log.h>
+
+#define private public
+#include <canrawview.h>
+#include <gui/crvguiinterface.h>
+#include <crvfactoryinterface.h>
+#undef private
+
+std::shared_ptr<spdlog::logger> kDefaultLogger;
 using namespace fakeit;
+
+
+
 
 TEST_CASE("Init table", "[canrawview]")
 {
 
+    
+    
     Mock<CRVGuiInterface> crvMock;
     Mock<CRVFactoryInterface> crvFactoryMock;
     
@@ -19,7 +33,35 @@ TEST_CASE("Init table", "[canrawview]")
     Fake(Dtor(crvMock));
 
 
-    When(Method(crvMock, frameView)).Do([&]() {std::cout << "chuj dupa" << std::endl; });
+    Fake(Method(crvMock, setClearCbk));
+    Fake(Method(crvMock, setDockUndockCbk));
+    Fake(Method(crvMock, setSectionClikedCbk));
+    Fake(Method(crvMock, setFilterCbk));
+    Fake(Method(crvMock, setModel));
+    Fake(Method(crvMock, initTableView));
+
+
+    When(Method(crvMock, isViewFrozen)).Return(false);
+    Fake(Method(crvMock, scrollToBottom));
+    Fake(Method(crvMock, getSortOrder));
+    Fake(Method(crvMock, getClickedColumn));
+    Fake(Method(crvMock, setSorting));
+    Fake(Method(crvMock, getWindowTitle));
+    Fake(Method(crvMock, isColumnHidden));
+
+    CanRawView canRawView{ CanRawViewCtx(&crvMock.get()) };
+    CHECK(canRawView.initTableView("", "") == false);
+    
+}
+
+
+
+TEST_CASE("Start simulation", "[canrawview]")
+{
+    Mock<CRVGuiInterface> crvMock;
+    Mock<CRVFactoryInterface> crvFactoryMock;
+
+    Fake(Dtor(crvMock));
 
     Fake(Method(crvMock, setClearCbk));
     Fake(Method(crvMock, setDockUndockCbk));
@@ -27,136 +69,21 @@ TEST_CASE("Init table", "[canrawview]")
     Fake(Method(crvMock, setFilterCbk));
     Fake(Method(crvMock, setModel));
     Fake(Method(crvMock, initTableView));
-    Fake(Method(crvMock, isViewFrozen));
-    Fake(Method(crvMock, scrollToBottom));
-    Fake(Method(crvMock, getSortOrder));
-    Fake(Method(crvMock, getClickedColumn));
-    Fake(Method(crvMock, setSorting));
-    Fake(Method(crvMock, getWindowTitle));
-    Fake(Method(crvMock, isColumnHidden));
+
+    Fake(Method(crvMock, startSimulation));
+
+    CanRawView canRawView{ CanRawViewCtx(&crvMock.get()) };
+    CHECK(canRawView.d_ptr->_simStarted == true)
 }
 
-
-
-/*
-TEST_CASE("Add and remove frame test", "[canrawsender]")
+int main(int argc, char* argv[])
 {
-    class helpTestClass {
-    public:
-        helpTestClass()
-        {
-            list.append(&item);
-            tvModel.appendRow(list);
-            auto idx = tvModel.indexFromItem(list.at(0));
-            indexList.append(idx);
-        }
-        QModelIndexList getList()
-        {
-            return indexList;
-        }
-
-    private:
-        QStandardItemModel tvModel;
-        QStandardItem item;
-        QList<QStandardItem*> list;
-        QModelIndexList indexList;
-    };
-
-    using namespace fakeit;
-    CRSGuiInterface::add_t addLineCbk;
-    CRSGuiInterface::remove_t removeLineCbk;
-
-    Mock<CRSGuiInterface> crsMock;
-
-    Mock<CheckBoxInterface> nlmCheckBoxMock;
-    Mock<LineEditInterface> nlmLineEditMock;
-    Mock<PushButtonInterface> nlmPushButtonMock;
-
-    Mock<NLMFactoryInterface> nlmFactoryMock;
-    Fake(Dtor(nlmFactoryMock));
-    helpTestClass mHelp;
-
-    Fake(Dtor(nlmLineEditMock));
-    Fake(Method(nlmLineEditMock, textChangedCbk));
-    Fake(Method(nlmLineEditMock, init));
-    Fake(Method(nlmLineEditMock, setPlaceholderText));
-    Fake(Method(nlmLineEditMock, setDisabled));
-    When(Method(nlmLineEditMock, getMainWidget)).AlwaysDo([&]() {
-        return reinterpret_cast<QWidget*>(&nlmLineEditMock.get());
-    });
-    When(Method(nlmFactoryMock, createLineEdit)).AlwaysDo([&]() { return &nlmLineEditMock.get(); });
-
-    Fake(Dtor(nlmCheckBoxMock));
-    Fake(Method(nlmCheckBoxMock, releasedCbk));
-    When(Method(nlmCheckBoxMock, getMainWidget)).Return(reinterpret_cast<QWidget*>(&nlmCheckBoxMock.get()));
-    When(Method(nlmFactoryMock, createCheckBox)).Return(&nlmCheckBoxMock.get());
-
-    Fake(Dtor(nlmPushButtonMock));
-    Fake(Method(nlmPushButtonMock, init));
-    Fake(Method(nlmPushButtonMock, pressedCbk));
-    When(Method(nlmPushButtonMock, getMainWidget)).Return(reinterpret_cast<QWidget*>(&nlmPushButtonMock.get()));
-    When(Method(nlmFactoryMock, createPushButton)).Return(&nlmPushButtonMock.get());
-
-    Fake(Dtor(crsMock));
-    When(Method(crsMock, setAddCbk)).Do([&](auto&& fn) { addLineCbk = fn; });
-    When(Method(crsMock, setRemoveCbk)).Do([&](auto&& fn) { removeLineCbk = fn; });
-    Fake(Method(crsMock, setDockUndockCbk));
-    Fake(Method(crsMock, getMainWidget));
-    Fake(Method(crsMock, initTableView));
-    When(Method(crsMock, getSelectedRows)).Do([&]() { return mHelp.getList(); });
-    Fake(Method(crsMock, setIndexWidget));
-
-    CanRawSender canRawSender{ CanRawSenderCtx(&crsMock.get(), &nlmFactoryMock.get()) };
-
-    CHECK(canRawSender.getLineCount() == 0);
-    addLineCbk();
-    CHECK(canRawSender.getLineCount() == 1);
-    removeLineCbk();
-    CHECK(canRawSender.getLineCount() == 0);
+    bool haveDebug = std::getenv("CDS_DEBUG") != nullptr;
+    kDefaultLogger = spdlog::stdout_color_mt("cds");
+    if (haveDebug) {
+        kDefaultLogger->set_level(spdlog::level::debug);
+    }
+    cds_debug("Staring canrawsender unit tests");
+    QApplication a(argc, argv); // QApplication must exist when contructing QWidgets TODO check QTest
+    return Catch::Session().run(argc, argv);
 }
-
-TEST_CASE("Can raw sender save configuration test", "[newlinemanager]")
-{
-    using namespace fakeit;
-
-    Mock<CRSGuiInterface> crsMock;
-    Fake(Dtor(crsMock));
-    Fake(Method(crsMock, setAddCbk));
-    Fake(Method(crsMock, setRemoveCbk));
-    Fake(Method(crsMock, setDockUndockCbk));
-    Fake(Method(crsMock, getMainWidget));
-    Fake(Method(crsMock, initTableView));
-    Fake(Method(crsMock, getSelectedRows));
-    Fake(Method(crsMock, setIndexWidget));
-
-    Mock<NLMFactoryInterface> nlmFactoryMock;
-    Fake(Dtor(nlmFactoryMock));
-
-    CanRawSender canRawSender{ CanRawSenderCtx(&crsMock.get(), &nlmFactoryMock.get()) };
-
-    QJsonObject json;
-    canRawSender.saveSettings(json);
-
-    CHECK(json.empty() == false);
-    CHECK(json.count() == 3);
-    const auto colIter = json.find("columns");
-    CHECK(colIter != json.end());
-    CHECK(colIter.value().type() == QJsonValue::Array);
-    const auto colArray = json["columns"].toArray();
-    CHECK(colArray.empty() == false);
-    CHECK(colArray.size() == 5);
-    CHECK(colArray.contains("Id") == true);
-    CHECK(colArray.contains("Data") == true);
-    CHECK(colArray.contains("Loop") == true);
-    CHECK(colArray.contains("Interval") == true);
-
-    CHECK(json.contains("content") == true);
-
-    const auto sortIter = json.find("sorting");
-    CHECK(sortIter != json.end());
-    CHECK(sortIter.value().type() == QJsonValue::Object);
-    const auto sortObj = json["sorting"].toObject();
-    CHECK(sortObj.contains("currentIndex") == true);
-}
-
-*/
